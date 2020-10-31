49654262745741cfeafda26e0f8f63ba84994b4c
adsalomo <adsalomo@gmail.com>
2020-10-31 10:54:55 -0500

    feat: se implementan validaciones formulario registro mecanicos

---
+++ src/app/models/mechanic.models.ts
@@ -0,0 +1,12 @@
+export default class Mechanic {
+    document: number;
+    documentTypeId: number;
+    firstName: string;
+    secondName: string;
+    surName: string;
+    secondSurName: string;
+    cellPhone: string;
+    address: string;
+    email: string;
+    state: string
+}
--- src/app/pages/mechanic/mechanic.component.ts
+++ src/app/pages/mechanic/mechanic.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import Mechanic from 'src/app/models/mechanic.models';
 
 @Component({
   selector: 'app-mechanic',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MechanicComponent implements OnInit {
 
+  mechanic: Mechanic = new Mechanic();
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  save(mechanicForm: NgForm) {
+    if (mechanicForm.invalid) {
+      Object.values(mechanicForm.controls)
+        .forEach(control => {
+          control.markAsTouched();
+        });
+      return;
+    }
   }
 
 }
